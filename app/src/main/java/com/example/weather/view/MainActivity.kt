fb6154f28601d18a7e419ffb0b8d03ce89ae1eb2
adevvv <adevvv@bk.ru>
2023-04-02 21:13:12 +0300

    getLifeData

--- app/src/main/java/com/example/weather/view/MainActivity.kt
+++ app/src/main/java/com/example/weather/view/MainActivity.kt
@@ -1,12 +1,106 @@
 package com.example.weather.view
 
+
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.weather.R
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.weather.databinding.ActivityMainBinding
+import com.example.weather.viewmodel.MainViewModel
+import java.util.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewmodel: MainViewModel
+
+    private lateinit var GET: SharedPreferences
+    private lateinit var SET: SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        GET = getSharedPreferences(packageName, MODE_PRIVATE)
+        SET = GET.edit()
+
+        viewmodel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val cName = GET.getString("cityName", "london")?.lowercase(Locale.getDefault())
+        binding.edtCityName.setText(cName)
+        viewmodel.refreshData(cName!!)
+
+        getLiveData()
+
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            binding.llData.visibility = View.GONE
+            binding.tvError.visibility = View.GONE
+            binding.pbLoading.visibility = View.GONE
+
+            val cityName = GET.getString("cityName", cName)?.lowercase(Locale.getDefault())
+            binding.edtCityName.setText(cityName)
+            viewmodel.refreshData(cityName!!)
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
+
+        binding.imgSearchCity.setOnClickListener {
+            val cityName = binding.edtCityName.text.toString()
+            SET.putString("cityName", cityName)
+            SET.apply()
+            viewmodel.refreshData(cityName)
+            getLiveData()
+            Log.i(TAG, "onCreate: " + cityName)
+        }
+
+    }
+
+    private fun getLiveData() {
+
+        viewmodel.weather_data.observe(this, Observer { data ->
+            data?.let {
+                binding.llData.visibility = View.VISIBLE
+
+
+                binding.tvCityName.text = data.name
+
+                binding.tvDegree.text = data.main.temp.toString() + "°C"
+
+                binding.tvHumidity.text = data.main.humidity.toString() + "%"
+                binding.tvWindSpeed.text = data.wind.speed.toString()
+                binding.tvLat.text = data.coord.lat.toString()
+                binding.tvLon.text = data.coord.lon.toString()
+
+            }
+        })
+
+        viewmodel.weather_error.observe(this, Observer { error ->
+            error?.let {
+                if (error) {
+                    binding.tvError.visibility = View.VISIBLE
+                    binding.pbLoading.visibility = View.GONE
+                    binding.llData.visibility = View.GONE
+                } else {
+                    binding.tvError.visibility = View.GONE
+                }
+            }
+        })
+
+        viewmodel.weather_loading.observe(this, Observer { loading ->
+            loading?.let {
+                if (loading) {
+                    binding.pbLoading.visibility = View.VISIBLE
+                    binding.tvError.visibility = View.GONE
+                    binding.llData.visibility = View.GONE
+                } else {
+                    binding.pbLoading.visibility = View.GONE
+                }
+            }
+        })
+
     }
 }
